b995734b48f94eb9c93e9c4150a40d62d8d18e63
LikhithIrani <167529333+LikhithIrani@users.noreply.github.com>
2026-02-26 13:26:31 +0530

    Update app.cpp

--- backend/app.cpp
+++ backend/app.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <cstring>
 
 int main() {
-    std::cout << "Hello from Jenkins CI/CD Lab!" << std::endl;
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+    sockaddr_in address;
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server_fd, (struct sockaddr*)&address, sizeof(address));
+    listen(server_fd, 3);
+
+    while (true) {
+        int addrlen = sizeof(address);
+        int new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+
+        std::string response =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Served by backend container\n";
+
+        send(new_socket, response.c_str(), response.size(), 0);
+        close(new_socket);
+    }
+
     return 0;
 }
